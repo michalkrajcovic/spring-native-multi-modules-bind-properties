615eca75d05932fda8b5159b85213726cd57ee0c
Michal Krajcovic <mkrajcovic@gohealth.com>
2022-11-10 09:33:37 +0100

    Initial commit

---
+++ greetings-configuration/src/main/java/com/example/greetings/configuration/GreetingsProperties.java
@@ -0,0 +1,19 @@
+package com.example.greetings.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@ConstructorBinding
+@ConfigurationProperties("greetings")
+public class GreetingsProperties {
+
+    private final String salute;
+
+    public GreetingsProperties(String salute) {
+        this.salute = salute;
+    }
+
+    public String getSalute() {
+        return salute;
+    }
+}
