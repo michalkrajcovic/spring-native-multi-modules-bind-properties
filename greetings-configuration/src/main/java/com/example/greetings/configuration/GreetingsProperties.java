80b4047f5d3ab00704a9f9a1d10043a8aff62d1d
Michal Krajcovic <mkrajcovic@gohealth.com>
2022-11-15 08:16:22 +0100

    Spring Boot 3.0.0-RC2

--- greetings-configuration/src/main/java/com/example/greetings/configuration/GreetingsProperties.java
+++ greetings-configuration/src/main/java/com/example/greetings/configuration/GreetingsProperties.java
@@ -1,14 +1,14 @@
 package com.example.greetings.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 
-@ConstructorBinding
 @ConfigurationProperties("greetings")
 public class GreetingsProperties {
 
     private final String salute;
 
+    @ConstructorBinding
     public GreetingsProperties(String salute) {
         this.salute = salute;
     }
